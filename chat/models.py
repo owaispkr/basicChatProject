8d888297e7cfe1dea7847ca57dfce55634523d80
owaispkr <owaispkr@users.noreply.github.com>
2020-08-14 10:33:39 +1200

    added room model, code is working fine

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    room = models.TextField()
+
+
+class Message(models.Model):
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.author.username
+
+    def last_10_messages(self):
+        return Message.objects.order_by('-timestamp').all()[:10]
